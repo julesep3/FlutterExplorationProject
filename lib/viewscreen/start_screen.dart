998c7619e3003c08f5025d80b20c945543f1810f
julesep3 <50899932+julesep3@users.noreply.github.com>
2021-08-25 16:36:01 -0500

    add profile picture to app

--- lib/viewscreen/start_screen.dart
+++ lib/viewscreen/start_screen.dart
@@ -9,6 +9,20 @@ class StartScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Start Menu'),
       ),
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: Image.asset(
+              'images/profile2.jpg',
+            ),
+            currentAccountPictureSize: Size(70.0, 70.0),
+            accountName: Text('julesep3'),
+            accountEmail: Text('julian@deerpeople.com'),
+          )
+        ],
+      )),
+      backgroundColor: Colors.blueGrey[60],
       body: Text('I am the body of the app.'),
     );
   }
