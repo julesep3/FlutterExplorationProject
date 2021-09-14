59d90669c18723167a3025167d029f8f8a0b36a5
julesep3 <50899932+julesep3@users.noreply.github.com>
2021-09-14 15:51:01 -0500

    reset startscreen using stateful widget

--- lib/viewscreen/start_screen.dart
+++ lib/viewscreen/start_screen.dart
@@ -1,50 +1,22 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
   static const routeName = '/startScreen';
 
+  @override
+  State<StatefulWidget> createState() {
+    return _StartScreenState();
+  }
+}
+
+class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Start Menu'),
+        title: Text('StartScreen'),
       ),
-      drawer: Drawer(
-          child: ListView(
-        children: [
-          UserAccountsDrawerHeader(
-            currentAccountPicture: Image.asset(
-              'images/profile2.jpg',
-            ),
-            currentAccountPictureSize: Size(70.0, 70.0),
-            accountName: Text('julesep3'),
-            accountEmail: Text('julian@deerpeople.com'),
-          ),
-          ListTile(
-            leading: Icon(Icons.badge_outlined),
-            title: Text('Work'),
-            onTap: () {
-              print('+++++++++++++++++++ drawer: Work');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.email_outlined),
-            title: Text('Email'),
-            onTap: () {
-              print('+++++++++++++++++++ drawer: Email');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.people_outline_sharp),
-            title: Text('Friends'),
-            onTap: () {
-              print('+++++++++++++++++++ drawer: Friends');
-            },
-          ),
-        ],
-      )),
-      backgroundColor: Colors.blueGrey[60],
-      body: Text('I am the body of the app.'),
+      body: Text('Body of the start screen'),
     );
   }
 }
